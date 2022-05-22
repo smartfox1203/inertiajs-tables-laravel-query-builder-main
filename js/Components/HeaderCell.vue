f34ab1b75d651cb64a94190fcd2a568bbd36d019
smartfox1203 <smartfox1203@gmail.com>
2022-05-22 09:58:23 +0900

    First commit

---
+++ js/Components/HeaderCell.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  props: {
+    cell: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  methods: {
+    onClick() {
+      if (this.cell.sortable) {
+        this.cell.onSort(this.cell.key);
+      }
+    },
+  },
+};
+</script>
